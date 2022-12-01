72c9a606a8b90049c64f131be125b193931a71ce
Arthur Akira <arthurakira.dev@gmail.com>
2022-12-01 13:33:41 -0300

    feat: add encryption to card cvv

--- src/modules/cards/domain/card-cvv.spec.ts
+++ src/modules/cards/domain/card-cvv.spec.ts
@@ -1,9 +1,13 @@
+import "dotenv/config";
+import Cryptr from "cryptr";
 import { randCreditCardCVV, randFullName } from "@ngneat/falso";
 import { Result } from "@core/logic/result";
 import { DomainErrors } from "@core/domain/domain-error";
 import { CardCVV } from "./card-cvv";
 
 describe("Card Security Code Value Object", () => {
+  const cryptr = new Cryptr(`${process.env.CRYPTR_SECRET}`);
+
   describe("Success", () => {
     it("Should be able to generate a Card CVV when its not provided", () => {
       const result = CardCVV.create();
@@ -12,26 +16,55 @@ describe("Card Security Code Value Object", () => {
       expect(result.isSuccess).toEqual(true);
       expect(result.value).toBeInstanceOf(CardCVV);
       expect(result.value?.value).toBeDefined();
-      expect(result.value?.value).toHaveLength(3);
+      expect(result.value?.decrypt()).toHaveLength(3);
+      expect(result.value?.value).not.toHaveLength(3);
     });
 
     it("Should be able to create a Card CVV when it is provided", () => {
       const cvv = randCreditCardCVV();
+      const encryptedCVV = cryptr.encrypt(cvv);
 
-      const result = CardCVV.create(cvv);
+      const result = CardCVV.create(encryptedCVV);
 
       expect(result).toBeInstanceOf(Result);
       expect(result.isSuccess).toEqual(true);
       expect(result.value).toBeInstanceOf(CardCVV);
-      expect(result.value?.value).toEqual(cvv);
+      expect(result.value?.value).toEqual(encryptedCVV);
+      expect(result.value?.decrypt()).toEqual(cvv);
+    });
+
+    it("Should be able to compare an external value to the CVV original value", () => {
+      const cvv = randCreditCardCVV();
+      const encryptedCVV = cryptr.encrypt(cvv);
+
+      const result = CardCVV.create(encryptedCVV);
+
+      expect(result).toBeInstanceOf(Result);
+      expect(result.isSuccess).toEqual(true);
+      expect(result.value).toBeInstanceOf(CardCVV);
+      expect(result.value?.value).toEqual(encryptedCVV);
+      expect(result.value?.decrypt()).toEqual(cvv);
+      expect(result.value?.compare(cvv)).toEqual(true);
     });
   });
 
   describe("Fail", () => {
-    it("Should return an error if the provided CVV is not a 3 numeric digits string", () => {
+    it("Should return an error if the provided CVV is not encrypted", () => {
       const invalidCVV = randFullName();
 
       const result = CardCVV.create(invalidCVV);
+
+      expect(result).toBeInstanceOf(DomainErrors.InvalidPropsError);
+      expect(result.value).toBeNull();
+      expect(result.error).toBeDefined();
+      expect(result.error?.message).toEqual("Invalid CVV encryption");
+    });
+
+    it("Should return an error if the provided CVV is correctly encrypted but with an invalid format", () => {
+      const invalidCVV = randFullName();
+      const encryptedCVV = cryptr.encrypt(invalidCVV);
+
+      const result = CardCVV.create(encryptedCVV);
 
       expect(result).toBeInstanceOf(DomainErrors.InvalidPropsError);
       expect(result.value).toBeNull();
--- src/modules/cards/domain/card-cvv.ts
+++ src/modules/cards/domain/card-cvv.ts
@@ -1,8 +1,11 @@
+import "dotenv/config";
 import { randCreditCardCVV } from "@ngneat/falso";
+
 import { ValueObject } from "@core/domain/value-object";
 import { Result } from "@core/logic/result";
 import { Either } from "@core/logic/either";
 import { DomainErrors } from "@core/domain/domain-error";
+import Cryptr from "cryptr";
 
 interface CardCVVProps {
   value: string;
@@ -13,6 +16,8 @@ type CreateCardCVVResult = Either<DomainErrors.InvalidPropsError, Result<CardCVV
 type ValidateCardCVVResult = Either<DomainErrors.InvalidPropsError, Result<string, null>>;
 
 export class CardCVV extends ValueObject<CardCVVProps> {
+  private static _cryptr: Cryptr = new Cryptr(`${process.env.CRYPTR_SECRET}`);
+
   private constructor(props: CardCVVProps) {
     super(props);
   }
@@ -21,16 +26,37 @@ export class CardCVV extends ValueObject<CardCVVProps> {
     return this.props.value;
   }
 
+  public compare(value: string): boolean {
+    const decryptedCVV = CardCVV._cryptr.decrypt(this.props.value);
+
+    return decryptedCVV === value;
+  }
+
+  public decrypt(): string {
+    return CardCVV._cryptr.decrypt(this.props.value);
+  }
+
+  private static encrypt(cvv: string): string {
+    return CardCVV._cryptr.encrypt(cvv);
+  }
+
   private static validate(cvv?: string): ValidateCardCVVResult {
     if (cvv === undefined) {
-      // When the CVV is not provided => generate random CVV
-      return Result.ok<string>(randCreditCardCVV());
+      const encryptedCVV = CardCVV.encrypt(randCreditCardCVV());
+
+      return Result.ok<string>(encryptedCVV);
     }
 
-    const cvvRegex = /^[0-9]{3}$/;
+    try {
+      const decryptedPassword = CardCVV._cryptr.decrypt(cvv);
 
-    if (!cvvRegex.test(cvv)) {
-      return DomainErrors.InvalidPropsError.create("Card CVV must be a 3 digits numeric string");
+      const cvvRegex = /^[0-9]{3}$/;
+
+      if (!cvvRegex.test(decryptedPassword)) {
+        return DomainErrors.InvalidPropsError.create("Card CVV must be a 3 digits numeric string");
+      }
+    } catch (err) {
+      return DomainErrors.InvalidPropsError.create("Invalid CVV encryption");
     }
 
     return Result.ok<string>(cvv);
@@ -44,7 +70,6 @@ export class CardCVV extends ValueObject<CardCVVProps> {
     }
 
     const cvvValue = cvvOrError.value!;
-
     const cardCVVEntity = new CardCVV({ value: cvvValue });
 
     return Result.ok<CardCVV>(cardCVVEntity);
